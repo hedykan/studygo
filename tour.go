4c2e78d0c500342e4266e17bf95e8a2804607bb3
hedykan <hedykan@outlook.com>
2021-03-19 13:54:07 +0800

    add total.md and update tour.go

--- tour.go
+++ tour.go
@@ -159,3 +159,119 @@ func (r MyReader) Read(b []byte) (int,error){
 func main() {
 	reader.Validate(MyReader{})
 }
+
+// 练习：rot13Reader
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func rot13(b byte) byte {
+	switch {
+	case 'A' <= b && b <= 'Z':
+		b = (b - 'A' + 13) % 26 + 'A'
+	case 'a' <= b && b <= 'z':
+		b = (b - 'a' + 13) % 26 + 'a'
+	}
+	return b
+}
+
+func (rot rot13Reader) Read(b []byte) (int,error){
+	n, err := rot.r.Read(b)
+	for i := 0; i < n; i++ {
+		b[i] = rot13(b[i])
+	}
+   return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
+
+// 练习：图像
+package main
+
+import (
+	"golang.org/x/tour/pic"
+	"image"
+	"image/color"
+)
+
+type Image struct{
+	w int
+	h int
+}
+
+func (i Image) Bounds() image.Rectangle {
+	return image.Rect(0,0,i.w,i.h)
+}
+
+func (i Image) ColorModel() color.Model {
+   return color.RGBAModel
+}
+
+func (i Image) At(x, y int) color.Color {
+	return color.RGBA{uint8(x),uint8(y),uint8(255),uint8(255)}
+}
+
+func main() {
+	m := Image{200, 200}
+	pic.ShowImage(m)
+}
+
+// 练习：等价二叉查找树
+package main
+
+import (
+	"golang.org/x/tour/tree"
+	"fmt"
+)
+
+// Walk 步进 tree t 将所有的值从 tree 发送到 channel ch。
+func Walk(t *tree.Tree, ch chan int) {
+	add(t, ch)
+	close(ch)
+}
+
+func add(t *tree.Tree, ch chan int) {
+	ch <- t.Value
+	if t.Left != nil {
+		add(t.Left, ch)
+	}
+	if t.Right != nil {
+		add(t.Right, ch)
+	}
+}
+
+// Same 检测树 t1 和 t2 是否含有相同的值。
+func Same(t1, t2 *tree.Tree) bool {
+	ch1 := make(chan int, 10)
+	ch2 := make(chan int, 10)
+	go Walk(t1, ch1)
+	go Walk(t2, ch2)
+	i := 0
+	for n := range ch1 {
+		fmt.Println(i)
+		i = i ^ n // 按位异或
+	}
+	fmt.Println()
+	for n := range ch2 {
+		fmt.Println(i)
+		i = i ^ n // 按位异或
+	}
+	return i == 0
+}
+
+func main() {
+	fmt.Println(Same(tree.New(1), tree.New(1)))
+	fmt.Println(Same(tree.New(2), tree.New(1)))
+}
